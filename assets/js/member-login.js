d77a04f8b31953fee19b1d5d6ca9dd9ec5298b13
Qi <18810651992@163.com>
2020-12-06 20:50:45 +0800

    12/6

---
+++ assets/js/member-login.js
@@ -0,0 +1,16 @@
+$('.active-two').click(function(){
+	$(".form-two").show();
+	$(".form-one").hide();
+	$(".active-two").css("background-color","#043458")
+	$(".active-two").css("color","#fff")
+	$(".active-one").css("background-color","#fff")
+	$(".active-one").css("color","#043458")
+})
+$('.active-one').click(function(){
+	$(".form-one").show();
+	$(".form-two").hide();
+	$(".active-one").css("background-color","#043458")
+	$(".active-one").css("color","#fff")
+	$(".active-two").css("background-color","#fff")
+	$(".active-two").css("color","#043458")
+})
